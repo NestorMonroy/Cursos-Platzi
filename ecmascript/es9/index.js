0e8358c4dec6a4285ee2408f7b954b849c1f3e19
NestorMonroy <nestor.monroy.90@gmail.com>
2020-11-19 16:58:40 -0600

    adding examples es9

---
+++ ecmascript/es9/index.js
@@ -0,0 +1,40 @@
+// operador de reposo
+
+const obj = {
+	name: 'nestor',
+	age: 30,
+	country: 'MX'
+};
+
+let { country, ...all } = obj;
+console.log(all);
+
+const obj = {
+	name: 'nestor',
+	age: 30
+};
+
+const obj1 = {
+	...obj,
+	country: 'MX'
+};
+console.log(obj1);
+
+const helloWorld = () => {
+	return new Promise((resolve, reject) => {
+		true ? setTimeout(() => resolve('hello', 3000)) : reject(new Error('error'));
+	});
+};
+
+helloWorld()
+	.then((response) => console.log(response))
+	.catch((err) => console.log(err))
+	.finally(() => console.log('finaliza'));
+
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/;
+const match = regexData.exec('2019-04-20');
+const year = match[1];
+const month = match[2];
+const day = match[3];
+
+console.log(year, month, day);
