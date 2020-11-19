7775e4273d561369926cc7ef3d6b7bb417950c05
NestorMonroy <nestor.monroy.90@gmail.com>
2020-11-19 16:58:52 -0600

    adding examples es10

---
+++ ecmascript/es10/index.js
@@ -0,0 +1,33 @@
+let array = [ 1, 2, 3, [ 1, 2, 3, [ 1, 2, 3 ] ] ];
+
+console.log(array.flat(2));
+
+let array = [ 1, 2, 3, 4, 5 ];
+console.log(array.flatMap((value) => [ value, value * 2 ]));
+
+let hello = '          hello';
+
+// elimina espacios de strings
+console.log(hello);
+console.log(hello.trimStart());
+
+let hello = 'hello      ';
+
+console.log(hello);
+console.log(hello.trimEnd());
+
+/* try {
+    
+} catch {
+    error
+}
+ */
+
+
+let entries = [['name','David'], ["age", 30]]
+console.log(Object.fromEntries(entries))
+
+let mySimbol = `My Symbol`
+let simbol = Symbol(mySimbol)
+console.log(simbol.desciption)
+
