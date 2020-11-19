ce811d934eb9e114216ad001646de522cd950c7f
NestorMonroy <nestor.monroy.90@gmail.com>
2020-11-19 16:58:09 -0600

    adding examples es7

---
+++ ecmascript/es7/index.js
@@ -0,0 +1,14 @@
+let numbers = [ 1, 2, 3, 9, 5 ];
+
+if (numbers.includes(7)) {
+	console.log('se encuentra');
+} else {
+	console.log('No se encuentra');
+}
+
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result);
+
