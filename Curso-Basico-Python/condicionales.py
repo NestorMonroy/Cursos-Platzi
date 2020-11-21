21d56198a5ba5508a5bede71c92f5df306659fc0
NestorMonroy <nestor.monroy.90@gmail.com>
2020-11-20 19:20:02 -0600

    adding examples python

---
+++ Curso-Basico-Python/condicionales.py
@@ -0,0 +1,15 @@
+# edad = int(input("escribe edad: "))
+# if edad > 17:
+#     print('mayor de edad')
+# else:
+#     print('menor')
+
+numero = int(input("escribe numero: "))
+
+if numero > 5:
+    print("mayor a 5")
+elif numero == 5:
+    print("es igual a 5")
+else:
+    print("es menor")
+
