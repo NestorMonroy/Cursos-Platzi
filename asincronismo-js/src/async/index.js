1f8d437316811760da85b3175e6df219a8fb58d9
NestorMonroy <nestor.monroy.90@gmail.com>
2020-11-24 13:13:34 -0600

    adding function whit async

---
+++ asincronismo-js/src/async/index.js
@@ -0,0 +1,27 @@
+const doSomethingAsync = () => {
+	return new Promise((resolve, reject) => {
+		true ? setTimeout(() => resolve('Do something Async'), 3000) : reject(new Error('test error'));
+	});
+};
+
+const doSomething = async () => {
+	const something = await doSomethingAsync();
+	console.log(something);
+};
+
+console.log('before');
+doSomething();
+console.log('after');
+
+const anotherFuncion = async () => {
+	try {
+		const something = await doSomethingAsync();
+		console.log(something);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+console.log('before 1');
+anotherFuncion();
+console.log('after 1');
