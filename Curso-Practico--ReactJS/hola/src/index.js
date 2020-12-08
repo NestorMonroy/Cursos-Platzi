687d7013b4a54ceb51c44b9eb6f19bf3cdb9572b
Nestor Monroy <nestor.monroy.90@gmail.com>
2020-12-07 23:04:16 -0600

    addig example react

--- Curso-Practico--ReactJS/hola/src/index.js
+++ Curso-Practico--ReactJS/hola/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HolaMundo from './components/HolaMundo';
+import Button from './components/Button'
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Button text="click 2 " />
+  
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
