b80d8b22e2276faea6eab2b11dd47be2122367b1
Nestor Monroy <nestor.monroy.90@gmail.com>
2020-12-10 19:10:07 -0600

    adding notes to nestorvideo

--- Curso-Practico--ReactJS/nestorvideo/webpack.config.js
+++ Curso-Practico--ReactJS/nestorvideo/webpack.config.js
@@ -1,39 +1,73 @@
+//path. En este caso es un módulo que ya existe en node.
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin')
+// instanciar los plugin 
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  //hacer referencia a nuestro archivo principal,
   entry: './src/index.js',
-  output: {
+  //output, que es donde vamos a guardar nuestros archivos resultantes cuando hagamos la compilación
+	output: {
+    //path -> decirle dónde se va a guardar. (resolve) nos va a permitir detectar el directorio del que nos encontramos, pasándole un directorio en el cual vamos a guardar los archivos
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    //filename para ponerle un nombre a nuestro archivo principal.
+		filename: 'bundle.js'
   },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {
+  // resolver las extensiones que vamos a utilizar para nuestro proyecto.
+	resolve: {
+		extensions: [ '.js', '.jsx' ]
+	},
+	module: {
+    //se crean reglas
+		rules: [
+			{
+        //test: Aquí vamos a pasarle una expresión regular que nos permita a nosotros identificar nuestros archivos de .js y de .jsx
         test: /\.(js|jsx)$/,
+        //pasar la exclusión de la carpeta de node_modules
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-
+        //se usa el loader instalado
+				use: {
+					loader: 'babel-loader'
+				}
       },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader'
-          }
-        ]
-      },
-    ]
+      //nueva regla que nos va a permitir a nosotros trabajar con los archivos de HTML.
+			{
+        //test: expresión regular que nos permita a nosotros identificar nuestros archivos html
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader'
+					}
+				]
+			},
+			{
+				test: /\.(s*)css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					'css-loader',
+					'sass-loader'
+				]
+			}
+		]
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './public/index.html',
-      filename: './index.html'
+  
+  // se hace referencia a los plugins instalados 
+
+	plugins: [
+    //en este caso "html-webpack-plugin"
+		new HtmlWebPackPlugin({
+      //es donde está ubicado el template que tenemos
+			template: './public/index.html',
+			filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css'
+
     })
-  ]
-}
+	]
+};
+
+
