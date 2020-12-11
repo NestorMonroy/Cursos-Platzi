97e8fbd2939919f86c5decff1d8243c9a4a70ac3
Nestor Monroy <nestor.monroy.90@gmail.com>
2020-12-10 19:17:40 -0600

    adding web-pack-loader and sass

--- Curso-Practico--ReactJS/nestorvideo/src/components/HelloWorld.jsx
+++ Curso-Practico--ReactJS/nestorvideo/src/components/HelloWorld.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import '../assets/styles/App.scss'
 
-const HelloWorld = () => {
+const HelloWorld = () => (
   <div>Hola</div>
-}
+)
 
 export default HelloWorld;
