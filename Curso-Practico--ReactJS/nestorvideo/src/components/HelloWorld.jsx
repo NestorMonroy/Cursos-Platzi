d2e8a9b34124602137045b77cc0135dc930f0d57
NestorMonroy <nestor.monroy.90@gmail.com>
2020-12-13 18:28:38 -0600

    change name repo

--- Curso-Practico--ReactJS/nestorvideo/src/components/HelloWorld.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import '../assets/styles/App.scss'
-
-const HelloWorld = () => (
-  <div>Hola</div>
-)
-
-export default HelloWorld;
--- Curso-Practico--ReactJS/nestorvideo/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld'
-/*
-  render recibe dos parámetros. 
-  Uno es el componente. Y el otro es dónde voy a empujar que este componente. Que en este caso se verá en un elemento particular del HTML  
-*/
-ReactDOM.render(<HelloWorld />, document.getElementById('app'))
