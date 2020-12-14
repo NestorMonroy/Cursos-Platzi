041a9ad47aba29fc856894063a518061654e02e1
NestorMonroy <nestor.monroy.90@gmail.com>
2020-12-13 19:38:55 -0600

    adding components and css, header, categories, carousel, carouselItem and footer

--- Curso-Practico-ReactJS/nestorvideo/src/index.js
+++ Curso-Practico-ReactJS/nestorvideo/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld'
+import App from './containers/App';
+
 /*
   render recibe dos parámetros. 
   Uno es el componente. Y el otro es dónde voy a empujar que este componente. Que en este caso se verá en un elemento particular del HTML  
 */
-ReactDOM.render(<HelloWorld />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
