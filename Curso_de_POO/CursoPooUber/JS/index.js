820ed676660ee1831e859037b40c4b266e073182
NestorMonroy <nestor.monroy.90@gmail.com>
2020-12-17 19:37:50 -0600

    adding example to inheritance javaScript and python

--- Curso_de_POO/CursoPooUber/JS/index.js
+++ Curso_de_POO/CursoPooUber/JS/index.js
@@ -1,3 +1,11 @@
-var car = new Car("AW45", new Account("Joel", "asads"));
-car.passenger = 4;
-car.printDataCar();
+// var car = new Car("AW45", new Account("Joel", "asads"));
+// car.passenger = 4;
+// car.printDataCar();
+
+var uberX = new UberX(
+  "ASZXCX",
+  new Account("nestor", "ASA"),
+  "OtraMarca",
+  "OtroModel"
+);
+uberX.printDataCar();
--- Curso_de_POO/CursoPooUber/Python/main.py
+++ Curso_de_POO/CursoPooUber/Python/main.py
@@ -1,6 +1,8 @@
 from car import Car
 from account import Account
 
+from uberX import UberX
+
 if __name__ == "__main__":
     print("Hola")
     # car = Car()
@@ -13,6 +15,10 @@ if __name__ == "__main__":
     # car2.driver = "Otro"
     # print(vars(car2))
     
-    car = Car("asas", Account("Joel", "XXAS"))
-    print(vars(car))
-    print(vars(car.driver))
+    # car = Car("asas", Account("Joel", "XXAS"))
+    # print(vars(car))
+    # print(vars(car.driver))
+    
+    uberX = UberX("xxcc", Account("Joel", "XXAS"), "Otro", "Marca")
+    print(vars(uberX))
+    print(vars(uberX.driver))
