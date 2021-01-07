df96d2b0ad9a945f02b0ed3a358c8fe4d4ae1b70
NestorMonroy <nestor.monroy.90@gmail.com>
2021-01-06 23:10:54 -0600

    updated ventas main

---
+++ python-practico/more_strings.py
@@ -0,0 +1,33 @@
+name = 'nestor'
+
+print(name.upper()) # convierte a mayusculas
+
+print(name.find('es')) # encuentra el indice en donde exite un patron qu e nosotros definimos 
+
+name.startswith('n')
+
+name.startswith('x')
+
+dir(name)
+
+"""
+Dunder methods: También conocidos como métodos mágicos (magic métodos) o special methods (métodos especiales).
+
+https://docs.python.org/3/reference/datamodel.html#special-method-names
+
+"""
+
+def myfunction():
+    """
+    Texto de ayuda
+    """
+    pass
+
+
+print(help(myfunction))
+
+
+
+
+
+
