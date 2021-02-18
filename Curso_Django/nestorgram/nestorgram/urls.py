f03a6aabaa6e83bcc0c264579611c458dbcc31c8
NestorMonroy <nestor.monroy.90@gmail.com>
2021-02-17 23:52:13 -0600

    adding models posts

--- Curso_Django/nestorgram/nestorgram/urls.py
+++ Curso_Django/nestorgram/nestorgram/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from django.contrib import admin
+
+from django.conf.urls.static import static
+from django.conf import settings
+
 from nestorgram import views as local_views
 from posts import views as posts_views
 
@@ -10,4 +14,6 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
 
     path('posts/',posts_views.list_posts),
-]
+
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
