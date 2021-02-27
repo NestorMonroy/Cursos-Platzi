d4eee57214da592de12de6b77a9f9e80df198075
NestorMonroy <nestor.monroy.90@gmail.com>
2021-02-26 18:49:38 -0600

    adding model user

--- Curso_Django-Avanzado/cride/cride/utils/models.py
+++ Curso_Django-Avanzado/cride/cride/utils/models.py
@@ -30,6 +30,37 @@ class CrideModel(models.Model):
         """Meta option."""
 
         abstract = True
-
+        
         get_latest_by = 'created'
         ordering = ['-created', '-modified']
+
+
+"""
+Ejemplo de clase proxi
+
+class Person(models.Model):
+    fist_name = models.ChardField()
+    last_name = models.ChardField()
+
+
+
+class MyPerson(Person):
+    class Meta:
+        proxy = True
+
+    def say_hello(name):
+        pass
+
+
+
+MyPerson.objects.all()
+nestor = MyPerson.objects.get(pk=1)
+nestor.say_hello('Joel') => esto es posible ya que agrega la funcionalidad
+
+
+rulo = Person.objects.get(pk=2)
+rulo.say_hello('Joel') => no es posible ya que say_hello solo vive dentro de MyPerson
+
+https://coffeebytes.dev/herencia-en-modelos-de-django/
+
+"""
