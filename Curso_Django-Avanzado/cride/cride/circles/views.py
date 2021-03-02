cc0c831049b660bac24184563db5d0b53da385af
NestorMonroy <nestor.monroy.90@gmail.com>
2021-03-01 21:28:13 -0600

    adding example serializer and validator to error send message when send POST

--- Curso_Django-Avanzado/cride/cride/circles/views.py
+++ Curso_Django-Avanzado/cride/cride/circles/views.py
@@ -2,30 +2,25 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+#Models
 from cride.circles.models import Circle
+#Serializer
+from cride.circles.serializers import CircleSerializer, CreateCircleSerializer
 
 
 @api_view(['GET'])
 def list_circles(request):
     circles = Circle.objects.filter(is_public=True)
-    data = [circle.name for circle in circles]
-
-
-    return Response(data)
+    serializer = CircleSerializer(circles, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['POST'])
 def create_circle(request):
-    name = request.data['name']
-    slug_name = request.data['slug_name']
-    about = request.data.get('about', '')
+    serializer = CreateCircleSerializer(data= request.data)
+    serializer.is_valid(raise_exception=True)
 
-    circle = Circle.objects.create(name=name, slug_name=slug_name, about=about)
+    circle = serializer.save()
 
-    data = {
-        'name': circle.name,
-        'slug_name': circle.slug_name,
-    }
-
-    return Response(data)
+    return Response(CircleSerializer(circle).data)
     
