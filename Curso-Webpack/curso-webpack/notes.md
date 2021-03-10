5051eef48e1191120bbae665f097990db545e8cc
NestorMonroy <nestor.monroy.90@gmail.com>
2021-03-09 21:01:31 -0600

    adding babel and update confing webpack

--- Curso-Webpack/curso-webpack/notes.md
+++ Curso-Webpack/curso-webpack/notes.md
@@ -19,3 +19,16 @@ se agrega a package.json el script
 se ejecuta ahora con npm run build
 
 Babel te permite hacer que tu código JavaScript sea compatible con todos los navegadores
+
+babel-loader => nos permite usar babel con webpack
+@babel/core => es babel en general
+@babel/preset-env => trae y te permite usar las ultimas características de JavaScript
+@babel/plugin-transform-runtime => te permite trabajar con todo el tema de asincronismo como ser async y await
+
+Debes crear el archivo de configuración de babel el cual tiene como nombre .babelrc
+
+npm install babel-loader @babel/core @babel/preset-env @babel/plugin-transform-runtime -D
+
+Se crea el archivo de configuración de babel el cual tiene como nombre .babelrc
+
+Se agregan nueva configuracion a webpack.config
--- Curso-Webpack/js-portfolio/webpack.config.js
+++ Curso-Webpack/js-portfolio/webpack.config.js
@@ -1,12 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js', // punto de entrada
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js', // nombre de la salida
-    },
-    resolve : {
-        extensions: ['.js']
-    }
+  entry: './src/index.js', // punto de entrada
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js', // nombre de la salida
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        // Test => declara que extensión de archivos aplicara el loader
+        test: /\.m?js$/, // LEE LOS ARCHIVOS CON EXTENSION .JS
+        // Use es un arreglo u objeto donde dices que loader aplicaras
+        use: {
+          loader: 'babel-loader'
+        },
+        // Exclude => permite omitir archivos o carpetas especificas
+        exclude: /node_modules/
+      }
+    ]
+  }
+
 }
