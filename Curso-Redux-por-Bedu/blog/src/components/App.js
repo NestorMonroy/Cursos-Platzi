2c3642997bcf0fc5265d1e29724456403e8355e4
NestorMonroy <nestor.monroy.90@gmail.com>
2021-03-10 23:20:32 -0600

    adding axios and promise

--- Curso-Redux-por-Bedu/blog/src/components/App.js
+++ Curso-Redux-por-Bedu/blog/src/components/App.js
@@ -1,45 +1,38 @@
-import { Component } from 'react';
-
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      usuarios: [
-        {
-          nombre: 'Nestor',
-          correo: 'nestor@platzi.com',
-          enlace: 'nestor.com'
-        },
-        {
-          nombre: 'Joel',
-          correo: 'joel@platzi.com',
-          enlace: ''
-        },
-        {
-          nombre: 'Otro',
-          correo: 'info@platzi.com',
-          enlace: 'platzi.com'
-        }
-      ]
+      usuarios: []
     }
   }
 
+  async componentDidMount() {
+    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+    //console.log('respuesta', respuesta.data)
+    this.setState({
+      usuarios: respuesta.data
+    });
+  }
+
   ponerFilas = () => this.state.usuarios.map((usuario) => (
-    <tr>
+    <tr key={usuario.id}>
       <td>
-        {usuario.nombre}
+        {usuario.name}
       </td>
       <td>
-        {usuario.correo}
+        {usuario.email}
       </td>
       <td>
-        {usuario.enlace}
+        {usuario.website}
       </td>
     </tr>
   ));
 
   render() {
+    //console.log(this.state.usuarios)
     return (
       <div className="margen">
         <table className="tabla">
