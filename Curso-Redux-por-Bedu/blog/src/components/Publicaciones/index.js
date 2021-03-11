788d432bf5d04d4e86a323661a1b81b1f24ff4fa
NestorMonroy <nestor.monroy.90@gmail.com>
2021-03-11 16:49:39 -0600

    new component publicaciones

---
+++ Curso-Redux-por-Bedu/blog/src/components/Publicaciones/index.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Publicaciones extends Component {
+  render() {
+    return (
+      <div>
+        { this.props.match.params.key}
+      </div>
+    );
+  }
+}
+
+export default Publicaciones;
--- Curso-Redux-por-Bedu/blog/src/components/Usuarios/Tabla.js
+++ Curso-Redux-por-Bedu/blog/src/components/Usuarios/Tabla.js
@@ -1,45 +1,51 @@
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Tabla = (props) => {
   // Cuando hay una funcion en un componente funcional , debe de empezar con const, aqui no se usa this
-	const ponerFilas = () => props.usuarios.map((usuario) => (
-		<tr key={ usuario.id }>
-			<td>
-				{ usuario.name }
-			</td>
-			<td>
-				{ usuario.email }
-			</td>
-			<td>
-				{ usuario.website }
-			</td>
-		</tr>
-	));
+  const ponerFilas = () => props.usuarios.map((usuario, key) => (
+    <tr key={usuario.id}>
+      <td>
+        {usuario.name}
+      </td>
+      <td>
+        {usuario.email}
+      </td>
+      <td>
+        {usuario.website}
+      </td>
+      <td>
+        <Link to={`/publicaciones/${key}`}>
+          <div className="eye-solid icon"></div>
+        </Link>
+      </td>
+    </tr>
+  ));
 
-	return (
-		<table className="tabla">
-			<thead>
-				<tr>
-					<th>
-						Nombre
+  return (
+    <table className="tabla">
+      <thead>
+        <tr>
+          <th>
+            Nombre
 					</th>
-					<th>
-						Correo
+          <th>
+            Correo
 					</th>
-					<th>
-						Enlace
+          <th>
+            Enlace
 					</th>
-				</tr>
-			</thead>
-			<tbody>
-				{ ponerFilas() }
-			</tbody>
-		</table>
-	)
+        </tr>
+      </thead>
+      <tbody>
+        {ponerFilas()}
+      </tbody>
+    </table>
+  )
 }
 
 const mapStateToProps = (reducers) => {
-	return reducers.usuariosReducer;
+  return reducers.usuariosReducer;
 };
 
 export default connect(mapStateToProps)(Tabla);
