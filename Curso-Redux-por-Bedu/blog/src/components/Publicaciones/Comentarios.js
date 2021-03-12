9449456061332b409f7310a8ea450848a3e16af3
NestorMonroy <nestor.monroy.90@gmail.com>
2021-03-11 22:36:07 -0600

    adding comentarios cargando error resultados

--- Curso-Redux-por-Bedu/blog/src/components/Publicaciones/Comentarios.js
+++ Curso-Redux-por-Bedu/blog/src/components/Publicaciones/Comentarios.js
@@ -1,20 +1,18 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import Spinner from '../General/Spinner';
 import Fatal from '../General/Fatal';
 
 const Comentarios = (props) => {
-  console.log('ca')
-  if (props.com_cargando) {
-    return <Spinner />
-  }
   if (props.com_error) {
     return <Fatal mensaje={props.com_error} />
   }
+  if (props.com_cargando && !props.comentarios.length) {
+    return <Spinner />
+  }
 
   const ponerComentarios = () => (
-    
     props.comentarios.map((comentario) => (
-      
       <li key={comentario.id}>
         <b><u>{comentario.email}</u></b>
         <br />
