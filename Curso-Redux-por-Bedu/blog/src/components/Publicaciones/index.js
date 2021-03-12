23c11dcd68964a9e49d3531487f2df81e774726e
NestorMonroy <nestor.monroy.90@gmail.com>
2021-03-11 20:31:10 -0600

    adding publicaciones data

--- Curso-Redux-por-Bedu/blog/src/components/Publicaciones/index.js
+++ Curso-Redux-por-Bedu/blog/src/components/Publicaciones/index.js
@@ -1,5 +1,7 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Spinner from '../General/Spinner';
+import Fatal from '../General/Fatal';
 
 import * as usuariosActions from '../../actions/usuariosActions';
 import * as publicacionesActions from '../../actions/publicacionesActions';
@@ -10,20 +12,87 @@ const { traerPorUsuario: publicacionesTraerPorUsuario } = publicacionesActions;
 class Publicaciones extends Component {
 
   async componentDidMount() {
+    const {
+      usuariosTraerTodos,
+      match: { params: { key } },
+      publicacionesTraerPorUsuario
+    } = this.props;
+
     if (!this.props.usuariosReducer.usuarios.length) {
-      await this.props.usuariosTraerTodos();
+      await usuariosTraerTodos();
     }
-    this.props.publicacionesTraerPorUsuario(this.props.match.params.key);
+    if (this.props.usuariosReducer.error) {
+      return;
+    }
+    if (!('publicaciones_key' in this.props.usuariosReducer.usuarios[key])) {
+      await publicacionesTraerPorUsuario(key);
+    }
   }
 
+  ponerUsuario = () => {
+    const {
+      match: { params: { key } },
+      usuariosReducer
+    } = this.props;
+
+    if (usuariosReducer.error) {
+      return <Fatal mensaje={usuariosReducer.error} />;
+    }
+    if (!usuariosReducer.usuarios.length || usuariosReducer.cargando) {
+      return <Spinner />
+    }
+
+    const nombre = usuariosReducer.usuarios[key].name;
+
+    return (
+      <h1>
+        Publicaciones de { nombre}
+      </h1>
+    );
+  };
+
+  ponerPublicaciones = () => {
+    const {
+      usuariosReducer,
+      usuariosReducer: { usuarios },
+      publicacionesReducer,
+      publicacionesReducer: { publicaciones },
+      match: { params: { key } }
+    } = this.props;
+
+    if (!usuarios.length) return;
+    if (usuariosReducer.error) return;
+    if (publicacionesReducer.cargando) {
+      return <Spinner />;
+    }
+    if (publicacionesReducer.error) {
+      return <Fatal mensaje={publicacionesReducer.error} />
+    }
+    if (!publicaciones.length) return;
+    if (!('publicaciones_key' in usuarios[key])) return;
+
+    const { publicaciones_key } = usuarios[key];
+    return publicaciones[publicaciones_key].map((publicacion) => (
+      <div
+        key={publicacion.id}
+        className='pub_titulo'
+        onClick={() => alert(publicacion.id)}
+      >
+        <h2>
+          {publicacion.title}
+        </h2>
+        <h3>
+          {publicacion.body}
+        </h3>
+      </div>
+    ));
+  };
+
   render() {
-    console.log(this.props);
     return (
       <div>
-        <h1>
-          Publicaciones de
-				</h1>
-        { this.props.match.params.key}
+        { this.ponerUsuario()}
+        { this.ponerPublicaciones()}
       </div>
     );
   }
