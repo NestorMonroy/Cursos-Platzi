dfc4b4cc9c5bb4910b511bae80a5700860b3db5c
NestorMonroy <nestor.monroy.90@gmail.com>
2021-03-11 22:14:11 -0600

    adding traerComentarios

---
+++ Curso-Redux-por-Bedu/blog/src/components/Publicaciones/Comentarios.js
@@ -0,0 +1,23 @@
+const Comentarios = (props) => {
+  return (
+    <ul>
+      <li>
+        hola
+			</li>
+      <li>
+        hola
+			</li>
+      <li>
+        hola
+			</li>
+      <li>
+        hola
+			</li>
+      <li>
+        hola
+			</li>
+    </ul>
+  );
+};
+
+export default Comentarios;
