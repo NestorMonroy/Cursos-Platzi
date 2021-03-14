20f186b019f8080f5316ff555ba6b2a683583fc9
NestorMonroy <nestor.monroy.90@gmail.com>
2021-03-14 16:09:21 -0600

    adding styles header

--- Curso-React-Router-y-Redux/Video/src/components/Header.jsx
+++ Curso-React-Router-y-Redux/Video/src/components/Header.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
-import { logoutRequest } from '../actions';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
 const Header = props => {
-  const { user } = props;
+  const { user, isLogin, isRegister } = props;
   const hasUser = Object.keys(user).length > 0;
+
   const handleLogout = () => {
     props.logoutRequest({});
   }
-
+  const headerClass = classNames('header', {
+    isLogin,
+    isRegister,
+  });
   return (
-    <header className='header'>
+    <header className={headerClass}>
       <Link to="/">
         <img className="header__img" src={logo} alt="Platzi Video" />
       </Link>
@@ -34,7 +38,7 @@ const Header = props => {
             : null
           }
           {hasUser ?
-            <li><a href="#logout" onClick={handleLogout} >Cerrar Sesión</a></li>
+            <li><a href="#logout" onClick={handleLogout}>Cerrar Sesión</a></li>
             :
             <li>
               <Link to="/login">
