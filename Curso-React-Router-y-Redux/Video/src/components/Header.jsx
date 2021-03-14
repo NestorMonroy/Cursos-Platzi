64c6b60b52c9df8e64e7a942f0e0cec6838c7f28
NestorMonroy <nestor.monroy.90@gmail.com>
2021-03-14 15:15:52 -0600

    adding gravatar

--- Curso-React-Router-y-Redux/Video/src/components/Header.jsx
+++ Curso-React-Router-y-Redux/Video/src/components/Header.jsx
@@ -2,31 +2,34 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
-
+import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
 const Header = props => {
-
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
 
   return (
-    <header >
+    <header>
       <Link to="/">
         <img className="header__img" src={logo} alt="Platzi Video" />
       </Link>
       <div className="header__menu">
         <div className="header__menu--profile">
-          
-            <img src='' /> :
-            <img alt="" />
-          
+          {hasUser ?
+            <img src={gravatar(user.email)} alt={user.email} /> :
+            <img src={userIcon} alt="" />
+          }
           <p>Perfil</p>
         </div>
         <ul>
-            <li><a href="/"></a></li> 
+          {hasUser ?
+            <li><a href="/">{user.name}</a></li>
             : null
-          
+          }
+          {hasUser ?
             <li><a href="#logout" >Cerrar Sesión</a></li>
             :
             <li>
@@ -34,13 +37,18 @@ const Header = props => {
                 Iniciar sesión
               </Link>
             </li>
-          
+          }
         </ul>
       </div>
     </header>
   );
 }
 
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
 
 
-export default Header;
+export default connect(mapStateToProps, null)(Header);
