0402756cfdc3b4be32ee679a6d8c1686584e4b7c
NestorMonroy <nestor.monroy.90@gmail.com>
2021-03-14 15:45:13 -0600

    adding player container

--- Curso-React-Router-y-Redux/Video/src/containers/Player.jsx
+++ Curso-React-Router-y-Redux/Video/src/containers/Player.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { getVideoSource } from '../actions';
 import '../assets/styles/components/Player.scss';
 import NotFound from '../containers/NotFound';
 
-const Player = props => {
-  const { id } = props.match.params;
+const Player = () => {
 
-  return hasPlaying ? (
+  return  (
     <div className="Player">
-
+      <video controls autoPlay>
+        <source  type="video/mp4" />
+      </video>
       <div className="Player-back">
-
+        <button type="button" >
+          Regresar
+        </button>
       </div>
     </div>
-  ) : <NotFound />;
+  )
 };
-
-
 
 export default Player;
