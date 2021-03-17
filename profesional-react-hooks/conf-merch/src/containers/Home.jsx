0a1f7c25017b07c6b65abeb31d0275b0d36f2cd9
NestorMonroy <nestor.monroy.90@gmail.com>
2021-03-16 20:40:56 -0600

    react helmet add

--- profesional-react-hooks/conf-merch/src/containers/Home.jsx
+++ profesional-react-hooks/conf-merch/src/containers/Home.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Products from '../components/Products';
 import initialState from '../initialState';
 
 const Home = () => {
   return (
-    <Products products={initialState.products} />
+    <>
+      <Helmet>
+        <title>Conf Merch</title>
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@NestorMonroy_" />
+        <meta name="twitter:creator" content="@NestorMonroy_" />
+        <meta name="twitter:title" content="Conf Store" />
+        <meta name="twitter:description" content="Conf Store" />
+        <meta
+          name="twitter:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:title" content="Conf Store" />
+        <meta property="og:description" content="Conf Store" />
+        <meta
+          property="og:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:url" content="store.xyz" />
+        <meta property="og:site_name" content="Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+      </Helmet>
+      <Products products={initialState.products} />
+    </>
   );
 }
 
