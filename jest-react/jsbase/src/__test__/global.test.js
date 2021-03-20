9bdd1313616b6c65a7281c3617750d61df043e9e
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-03-20 12:24:05 -0600

    [add-test]  boleand and array

--- jest-react/jsbase/src/__test__/global.test.js
+++ jest-react/jsbase/src/__test__/global.test.js
@@ -3,3 +3,32 @@ const text = "Hola Nestor";
 test("Debe contener un texto", () => {
   expect(text).toMatch(/Nestor/);
 });
+
+const fruits = ["manzana", "pera", "banana", "fresa"];
+
+// Test arrays
+test("It should has manzana", () => {
+  expect(fruits).toContain("manzana");
+});
+
+// Test numbers
+test("It should be greater than", () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+// Test Booleans
+test("It should be true", () => {
+  expect(true).toBeTruthy();
+});
+
+// Reverse strings function
+const reverseString = (str, cb) => {
+  cb(str.split("").reverse().join(""));
+};
+
+// Test callbacks
+test("The callback should return a reversed word", () => {
+  reverseString("Platzi", (str) => {
+    expect(str).toBe("iztalP");
+  });
+});
