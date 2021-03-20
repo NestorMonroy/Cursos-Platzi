714755531f2045518b8afc34c152946174f084f7
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-03-19 20:35:42 -0600

    [update-test] async/await add error when api fail

--- js-jest-2019/nestortest/__test__/async.test.js
+++ js-jest-2019/nestortest/__test__/async.test.js
@@ -1,14 +1,27 @@
 import { getDataFromApi } from "../promises";
 
 describe("Probar Async/Await", () => {
-  test("Realizar una petición a una API", async () => {
+  test("Realizar una peticion a una api", async () => {
     const api = "https://rickandmortyapi.com/api/character/";
     const getRick = "https://rickandmortyapi.com/api/character/1";
+    await getDataFromApi(api).then((data) => {
+      expect(data.results.length).toBeGreaterThan(0);
+    });
+    await getDataFromApi(getRick).then((data) => {
+      expect(data.name).toEqual("Rick Sanchez");
+    });
+  });
 
-    const data = await getDataFromApi(api);
-    expect(data.results.length).toBeGreaterThanOrEqual(0);
+  test("Realizando una peticion a una api con error", async () => {
+    const apiError = "http://httpstat.us/500";
+    const peticion = getDataFromApi(apiError);
+    await expect(peticion).rejects.toEqual(
+      Error("Request failed with status code 500")
+    );
+  });
 
-    const data2 = await getDataFromApi(getRick);
-    expect(data2.name).toEqual("Rick Sanchez");
+  test("Resuelve un Hola", async () => {
+    await expect(Promise.resolve("Hola")).resolves.toBe("Hola");
+    await expect(Promise.reject("Error")).rejects.toBe("Error");
   });
 });
