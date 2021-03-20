b46c242c2fa43c4087797ecab09f4a623d6bf135
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-03-19 19:05:09 -0600

    [add] test toEqual

---
+++ js-jest-2019/nestortest/__test__/matchers.test.js
@@ -0,0 +1,16 @@
+describe("Comparadores comunes", () => {
+  const user = {
+    name: "nestor",
+    lastname: "monroy",
+  };
+  const user2 = {
+    name: "nestor2",
+    lastname: "monroy2",
+  };
+  test("igualdad de elementos", () => {
+    expect(user).toEqual(user2);
+  });
+  test("No son exactamente iguales", () => {
+    expect(user).not.toEqual(user2);
+  });
+});
