ca87d4007aeb684d322cfd73a2600be2df66dbdc
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-03-21 14:57:51 -0600

    [add] snapshot

--- jest-react/store/src/__test__/components/Footer.test.js
+++ jest-react/store/src/__test__/components/Footer.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { mount } from "enzyme";
-
+import { create } from 'react-test-renderer';
 import Footer from "../../components/Footer";
 
 describe("<Footer />", () => {
@@ -12,5 +12,19 @@ describe("<Footer />", () => {
 
   test("Render of Footer Title", () => {
     expect(footer.find(".Footer-title").text()).toEqual("Store");
+  });
+});
+
+describe("Footer Snapshot", () => {
+  test("Comprobar la UI del componente Footer", () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
+  });
+});
+
+describe('Footer Snapshot', () => {
+  test('Comprobar la UI del componente Footer', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
--- jest-react/store/src/__test__/components/Header.test.js
+++ jest-react/store/src/__test__/components/Header.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { mount, shallow } from "enzyme";
 import ProviderMock from "../../__mocks__/ProviderMock";
 import Header from "../../components/Header";
-
+import { create } from 'react-test-renderer';
 // mount --> Cuando necesitas el DOM
 // shallow --> Solo necesitas algo particular del componente. No ocupas todo el DOM
 describe("<Header />", () => {
@@ -21,5 +21,16 @@ describe("<Header />", () => {
       </ProviderMock>
     );
     expect(header.find(".Header-title").text()).toEqual("Store");
+  });
+});
+
+describe('Header SnapShot', () => {
+  test('Comprobar el Snapshot de Header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.toJSON()).toMatchSnapshot();
   });
 });
