7afe2bd45a246dce6a5d69e1c9c77d0eeedc2d92
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-03-23 17:32:45 -0600

    [applying] loading strategies

--- pwa/pwa/src/service-worker.js
+++ pwa/pwa/src/service-worker.js
@@ -6,6 +6,37 @@
 
 // Precarga la app
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-//workbox.precaching.suppressWarnings();
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
+// App Shell
+workbox.routing.registerNavigationRoute("/index.html");
+
+// La API usa Stale While Revalidate para mayor velocidad
+workbox.routing.registerRoute(
+  /^https?:\/\/www.themealdb.com\/api\/.*/,
+  new workbox.strategies.StaleWhileRevalidate(),
+  "GET"
+);
+
+// Last fuentes van con Cache First y vencen al mes
+workbox.routing.registerRoute(
+  /^https:\/\/fonts.(?:googleapis|gstatic).com\/(.*)/,
+  workbox.strategies.cacheFirst({
+    cacheName: "google-fonts-cache",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  }),
+  "GET"
+);
+
+// Todo lo demás usa Network First // Por defecto, va al final de todo
+
+workbox.routing.registerRoute(
+  /^https?.*/,
+  workbox.strategies.networkFirst(),
+  "GET"
+);
