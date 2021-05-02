be1df58a6a4a6e64174fdfdc8ab3c7de64439e6d
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-05-02 11:13:02 -0500

    [update] feach

--- curso-next.js/next-js/pages/index.tsx
+++ curso-next.js/next-js/pages/index.tsx
@@ -1,26 +1,27 @@
-import React from "react";
-import Layout from "@components/Layout/Layout";
-import KawaiiHeader from "@components/KawaiiHeader/KawaiiHeader";
-import ProductList from "@components/ProductList/ProductList";
+import React from 'react'
+import fetch from 'isomorphic-unfetch'
+
+import Layout from '@components/Layout/Layout'
+import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
+import ProductList from '@components/ProductList/ProductList'
 
 const HomePage = () => {
-  const [productList, setProductList] = React.useState<TProduct[]>([]);
+  const [productList, setProductList] = React.useState<TProduct[]>([])
 
   React.useEffect(() => {
-    window
-      .fetch("/api/avo")
+    fetch('/api/avo')
       .then((response) => response.json())
       .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data);
-      });
-  }, []);
+        setProductList(data)
+      })
+  }, [])
 
   return (
     <Layout>
       <KawaiiHeader />
       <ProductList products={productList} />
     </Layout>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
