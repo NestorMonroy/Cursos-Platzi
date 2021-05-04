4dc7daa6617d57eb214136c8dbeba8ca4c940954
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-05-04 00:45:25 -0500

    [add] CartContext

--- gatsby/Gatsby-ecommerce/src/components/header.jsx
+++ gatsby/Gatsby-ecommerce/src/components/header.jsx
@@ -1,32 +1,41 @@
+import React from "react";
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
-import React from "react";
 import { MenuItem, StyledHeader } from "../styles/components";
+import { CartContext } from "../context";
 
-const Header = ({ siteTitle }) => (
-  <StyledHeader>
-    <Link to="/">
-      <img src="https://i.postimg.cc/6q3pg48v/Logo.png" alt="logoplatziswag" />
-    </Link>
-    <nav>
-      <ul>
-        <MenuItem margin>
-          <Link to="/">Productos</Link>
-        </MenuItem>
-        <MenuItem>
-          <Link to="/cart">
-            <span>
-              <img
-                src="https://i.postimg.cc/L6wpMxLt/cart.png"
-                alt="cartlogo"
-              />
-            </span>
-          </Link>
-        </MenuItem>
-      </ul>
-    </nav>
-  </StyledHeader>
-);
+const Header = ({ siteTitle }) => {
+  const { cart } = React.useContext(CartContext);
+
+  return (
+    <StyledHeader>
+      <Link to="/">
+        <img
+          src="https://i.postimg.cc/6q3pg48v/Logo.png"
+          alt="logoplatziswag"
+        />
+      </Link>
+      <nav>
+        <ul>
+          <MenuItem margin>
+            <Link to="/">Productos</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link to="/cart">
+              <span>
+                <img
+                  src="https://i.postimg.cc/L6wpMxLt/cart.png"
+                  alt="cartlogo"
+                />
+              </span>
+              {cart.length}
+            </Link>
+          </MenuItem>
+        </ul>
+      </nav>
+    </StyledHeader>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
