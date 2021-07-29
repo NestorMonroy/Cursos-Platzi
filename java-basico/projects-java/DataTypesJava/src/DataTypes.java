8b0e0a5165a97280f230c021001cc3a8ec68513d
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-07-29 01:34:59 -0500

    [Add] DataTypes Project

---
+++ java-basico/projects-java/DataTypesJava/src/DataTypes.java
@@ -0,0 +1,9 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int n = 1234567890;
+        long nL = 123456789001L;
+        double nD = 123.4561786957854;
+        float nF= 1.23456F;
+
+    }
+}
