24c7144c28e9cc8aa8679eea7bbe666498f42d77
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-07-30 01:05:53 -0500

    [add] example ForEach

--- java-basico/notes.md
+++ java-basico/notes.md
@@ -808,3 +808,39 @@ Para guardar un valor en alguna posición de nuestro array solo debemos usar el 
 ```java
 nombreVariable[indice] = valor;
 ```
+
+### Ciclos For anidados
+
+Los ciclos FOR nos ayudan a ejecutar una parte de nuestro código todas las veces que sean necesarias hasta que una condición se cumpla, por ejemplo, que un número supere o iguale cierta cantidad.
+
+Eso es exactamente lo que necesitamos para trabajar con índices. En vez de escribir todos los números a mano, vamos a utilizar un ciclo para imprimir el valor de cada posición de nuestros arreglos, incluso si estos son multidimensionales:
+
+```java
+// Array de una sola dimensión:
+for (int i = 0; i <= 3; i++) {
+  System.out.println(i);
+}
+// El resultado será: 0, 1, 2, 3
+
+// Array de dos dimensiones:
+for (int row = 0; row < cities.length; row++) {
+  for (int col = 0; col < cities[row].length; col++) {
+    System.out.println(cities[row][col]);
+  }
+}
+// El resultado será:
+// Colombia
+// Bogotá
+// México
+// Guadalajara
+// España
+// Madrid
+```
+
+El ciclo FOREACH también nos ayuda a recorrer los elementos de un array posición por posición, solo que no tenemos control sobre el índice, el ciclo se encarga de recorrer todo el array automáticamente:
+
+```java
+for (TipoDato elemento : coleccion) {
+  // Instrucciones
+}
+```
--- java-basico/projects-java/HolaMundo/src/Arrays.java
+++ java-basico/projects-java/HolaMundo/src/Arrays.java
@@ -1,11 +1,11 @@
 public class Arrays {
     public static void main(String[] args) {
         String[] androidVersions = new String[17]; // Se tiene que definir el tamano del arreglo
-        String days[] = new  String[7];
+        int days[] = new  int[7];
 
         String[][] cities = new String[4][2]; // 4*2 = 8
         /*
-        *  -------------------------
+        * -------------------------
         * |Country    | City       |
         * --------------------------
         * |Mexico     | CDMX       |
@@ -22,11 +22,31 @@ public class Arrays {
         androidVersions[1] = "Bannana Bread";
         androidVersions[2] = "Cupcake";
         androidVersions[3] = "Donut";
+                                //17
+        for (int i = 0; i < androidVersions.length ; i++) {
+            System.out.println(androidVersions[i]);
+        }
+
+        System.out.println();
+        System.out.println();
+        
+        for (String androidVersion : androidVersions) {
+            System.out.println(androidVersion);
+        }
+
+        /*
+
+
+        for (int i = 0; i < days.length ; i++) {
+            System.out.println(i);
+        }
 
         System.out.println(androidVersions[0]);
         System.out.println(androidVersions[1]);
         System.out.println(androidVersions[2]);
         System.out.println(androidVersions[3]);
+
+        */
 
         System.out.println();
         System.out.println();
@@ -40,6 +60,8 @@ public class Arrays {
         cities[3][0] = "Mexico";
         cities[3][1] = "CDMX";
 
+        /*
+
         System.out.println(cities[0][0]);
         System.out.println(cities[0][1]);
         System.out.println(cities[1][0]);
@@ -49,11 +71,42 @@ public class Arrays {
         System.out.println(cities[3][0]);
         System.out.println(cities[3][1]);
 
+        
+
+        for (int i = 0; i < cities.length; i++) {
+            for (int j = 0; j < cities[i].length; j++) {
+                System.out.println(cities[i][j]);
+            }
+        }
+        
+        */
+        //Return numero de id 
+        for (String[] pair:cities) {
+            for (String name: pair){
+                System.out.println(name);
+            }
+            
+        }
+
         String[][][][] animals = new String[2][3][2][2];
         animals[1][0][0][1] = "Monkey";
-        System.out.println();
-        System.out.println();
+        /*
         System.out.println(animals[1][0][0][1]);
+         */
+        System.out.println();
+        System.out.println();
+        
+        for (int i = 0; i <= 1 ; i++) {
+            for (int j = 0; j <=0 ; j++) {
+                for (int k = 0; k <= 0; k++) {
+                    for (int l = 0; l <=1 ; l++) {
+                        System.out.println(animals[i][j][k][l]);
+                    }
+
+                }
+
+            }
+        }
 
     }
 }
