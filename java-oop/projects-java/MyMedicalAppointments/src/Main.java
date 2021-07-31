3eb6b5f94d057828954978ffe2044bf58a1ae838
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-07-31 00:05:57 -0500

    [add] example Variable vs. Objeto

--- java-oop/projects-java/MyMedicalAppointments/src/Main.java
+++ java-oop/projects-java/MyMedicalAppointments/src/Main.java
@@ -7,6 +7,34 @@ public class Main {
         System.out.println(myDoctor.name);
         System.out.println(myDoctor.speciality);
 
+        int i = 0;
+        int b = 2;
+        b= i;
+
+        System.out.println();
+        System.out.println();
+
+        Patient patien = new Patient("Joel", "joel@abc");
+        Patient patien2 = new Patient("Berna", "berna@abc");
+
+        System.out.println(patien);
+        System.out.println(patien2);
+        System.out.println();
+        System.out.println();
+        System.out.println(patien.getName());
+        System.out.println(patien2.getName());
+
+        patien2 = patien;
+        System.out.println();
+        System.out.println();
+        System.out.println(patien.getName());
+        System.out.println(patien2.getName());
+        System.out.println();
+        System.out.println();
+        patien2.setName("Nuevo 1");
+        System.out.println(patien.getName());
+        System.out.println(patien2.getName());
+
     }
 
 }
--- java-oop/projects-java/MyMedicalAppointments/src/Patient.java
+++ java-oop/projects-java/MyMedicalAppointments/src/Patient.java
@@ -9,9 +9,18 @@ public class Patient {
     Double height;
 
 
+
     Patient(String name, String email){
         this.name = name;
         this.email = email;
     }
-    
+
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 }
