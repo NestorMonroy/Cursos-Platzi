ebbfeae228d978695d28bb03d04d23f41ee65c6a
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-07-31 01:10:11 -0500

    [add] example Nested Classes

--- java-oop/projects-java/MyMedicalAppointments/src/Doctor.java
+++ java-oop/projects-java/MyMedicalAppointments/src/Doctor.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
 
     static int globalID = 0; // Autoincrement
-    String name;
-    String email;
-    String speciality;
+    private String name;
+    private String email;
+    private String speciality;
 
     Doctor(String name, String speciality){
         System.out.println("Construyendo el objecto doctor");
@@ -25,5 +28,54 @@ public class Doctor {
 
     public void showId(){
         System.out.println("ID doctor: "+ globalID);
+    }
+
+    //Solo la clase de doctor puede generar citas
+    //Metodo que agrega citas
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time){
+        //availableAppointments.add(new AvailableAppointment(date,time));
+        availableAppointments.add(new Doctor.AvailableAppointment(date, time));
+    };
+
+    //Metodo que regresa la lista de citas
+    public ArrayList<AvailableAppointment> getAvailableAppointments() {
+        return availableAppointments;
+    }
+
+    public static class AvailableAppointment{
+        private int id_availableAppointment;
+        private Date date;
+        private String time;
+
+
+        public AvailableAppointment(Date date, String time){
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId_availableAppointment() {
+            return id_availableAppointment;
+        }
+
+        public void setId_availableAppointment(int id_availableAppointment) {
+            this.id_availableAppointment = id_availableAppointment;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- java-oop/projects-java/MyMedicalAppointments/src/Main.java
+++ java-oop/projects-java/MyMedicalAppointments/src/Main.java
@@ -1,25 +1,35 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
         //showMenu();
-//        Doctor myDoctor = new Doctor("Nestor Monroy", "Cardiología");
-//        System.out.println(myDoctor.name);
-//        System.out.println(myDoctor.speciality);
-//
+        Doctor myDoctor = new Doctor("Nestor Monroy", "Cardiología");
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
+
+        //System.out.println(myDoctor.getAvailableAppointments()); // Regresa el objecto
+
+        for (Doctor.AvailableAppointment aA:myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
+
+        
 //        int i = 0;
 //        int b = 2;
 //        b= i;
 
-        System.out.println();
-        System.out.println();
-
-        Patient patien = new Patient("Joel", "joel@abc.com");
-        patien.setWeight(50.6);
-        System.out.println(patien.getWeight());
-
-        patien.setPhoneNumber("1");
-        System.out.println(patien.getPhoneNumber());
+//        System.out.println();
+//        System.out.println();
+//
+//        Patient patien = new Patient("Joel", "joel@abc.com");
+//        patien.setWeight(50.6);
+//        System.out.println(patien.getWeight());
+//
+//        patien.setPhoneNumber("1");
+//        System.out.println(patien.getPhoneNumber());
 
         //Patient patien2 = new Patient("Berna", "berna@abc.com");
 
