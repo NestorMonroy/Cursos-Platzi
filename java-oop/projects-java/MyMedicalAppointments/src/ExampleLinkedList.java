5b726f0510bfdcb261ed6dcba906f070164b9118
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-07-31 03:56:24 -0500

    [add] example LinkedList

---
+++ java-oop/projects-java/MyMedicalAppointments/src/ExampleLinkedList.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class ExampleLinkedList {
+    public static void main(String[] args) {
+        LinkedList<String> personas = new LinkedList<String>();
+        personas.add("Luis0");
+        personas.add("Luis1");
+        personas.add("Luis2");
+        personas.add("Luis3");
+        personas.add("Luis4");
+
+        System.out.println(personas.size());
+        //Cambiar la posicion del iterador con ListIterator
+        ListIterator<String> it = personas.listIterator();
+        it.next();
+        it.add("Luis5");
+
+        for (String persona: personas) {
+            System.out.println(persona);
+        }
+    }
+
+}
