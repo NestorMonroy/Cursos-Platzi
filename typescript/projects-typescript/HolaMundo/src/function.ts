220d522064c1bb5435d05a76c87c6d49ad1c5dbf
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-08-02 12:38:13 -0500

    [add] example function with return type

--- typescript/projects-typescript/HolaMundo/src/function.ts
+++ typescript/projects-typescript/HolaMundo/src/function.ts
@@ -5,12 +5,12 @@
 // }
 
 // Usamos TS, definimos tipos para parametros
-type SqueareSize ='100x100' | '500x500' | '1000x10000'
+type SqueareSize = '100x100' | '500x500' | '1000x10000'
 
 // function createPicture(title:string, date:string, size:SqueareSize){
 //     //Se crea la fotografia
 //     console.log('Create Picture using',title,date, size );
-    
+
 // }
 
 // createPicture('My Birthday', '2020-03-10', '500x500');
@@ -19,21 +19,21 @@ type SqueareSize ='100x100' | '500x500' | '1000x10000'
 
 
 //Parametros opcionales 
-function createPicture(title?:string, date?:string, size?:SqueareSize){
+function createPicture(title?: string, date?: string, size?: SqueareSize) {
     //Se crea la fotografia
-    console.log('Create Picture using',title,date, size );
-    
+    console.log('Create Picture using', title, date, size);
+
 }
 
 createPicture('My Birthday', '2020-03-10', '500x500');
-createPicture('Colombia Trip', '2021-5-20'); 
-createPicture('Colombia Trip'); 
-createPicture(); 
+createPicture('Colombia Trip', '2021-5-20');
+createPicture('Colombia Trip');
+createPicture();
 
 
 // Flat Array function
 
-let createPic = (title:string, date:string, size:SqueareSize):object => {
+let createPic = (title: string, date: string, size: SqueareSize): object => {
 
     // return{
     //     title: title,
@@ -41,11 +41,30 @@ let createPic = (title:string, date:string, size:SqueareSize):object => {
     //     size:size
     // };
 
-    return{ title, date,size}    
+    return { title, date, size }
 
 };
 
-const picture = createPic('Ejemplo 00', '2020-03-25','1000x10000')
+const picture = createPic('Ejemplo 00', '2020-03-25', '1000x10000')
 console.log('picture', picture);
 
-// 
+
+// Tipo de retorno con TypeScript
+
+function handleError(code: number, message: string): never | string {
+    //Procesamiento del codigo. mensaje
+    if (message == 'error') {
+        throw new Error(`${message}. Code error: ${code}`)
+    } else {
+        return 'An error has ocurred'
+    }
+}
+
+try {
+    let result = handleError(200, 'OK') //String
+    console.log('result', result);
+    result = handleError(404, 'Error'); // nerver
+    console.log('result', result);
+} catch (error) {
+
+}
