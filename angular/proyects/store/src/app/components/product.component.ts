dfbf6e93170a1f4f3c469c09fc6674f7584159f9
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-08-11 01:44:39 -0500

    [add] styles product componet

--- angular/proyects/store/src/app/components/product.component.ts
+++ angular/proyects/store/src/app/components/product.component.ts
@@ -13,6 +13,7 @@ import { Product } from '../product.model';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product: Product;
