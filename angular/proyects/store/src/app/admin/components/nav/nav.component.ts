4cbfb762cd8cd15d49148892a7c5091d22c6b061
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-08-26 02:09:16 -0500

    [update] styles to admin/create

--- angular/proyects/store/src/app/admin/components/nav/nav.component.ts
+++ angular/proyects/store/src/app/admin/components/nav/nav.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, ChangeDetectorRef, OnDestroy} from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
+
+import {MediaMatcher} from '@angular/cdk/layout';
+
+
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -9,6 +14,16 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  @ViewChild('sidenav') sidenav: MatSidenav;
+  reason = '';
+
+  close(reason: string) {
+    this.reason = reason;
+    this.sidenav.close();
+  }
+
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
