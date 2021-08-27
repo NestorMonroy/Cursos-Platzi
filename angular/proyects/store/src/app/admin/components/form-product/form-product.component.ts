69e54d6690e16f212bbc4ddf209e65912fcad559
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-08-27 02:36:06 -0500

    [add] create form product

---
+++ angular/proyects/store/src/app/admin/components/form-product/form-product.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+import {ProductsService} from "../../../core/services/products/products.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-form-product',
+  templateUrl: './form-product.component.html',
+  styleUrls: ['./form-product.component.scss']
+})
+export class FormProductComponent implements OnInit {
+
+  form: FormGroup;
+
+
+  constructor(
+    private formBuilder:FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
+
+  ) {
+    this.buildForm();
+  }
+
+  ngOnInit(): void {
+    console.log('hola')
+  }
+
+  saveProduct(event:Event){
+    event.preventDefault() // para que no recarge la pagina **
+    console.log(this.form.value)
+    if(this.form.valid){
+      const product = this.form.value;
+      this.productsService.createProduct(product)
+        .subscribe((newProduct) => {
+          console.log(newProduct)
+          this.router.navigate(['./admin/products'])
+        })
+    }
+  }
+
+  private buildForm(){
+    this.form = this.formBuilder.group({
+      id: ['', [Validators.required]],
+      title: ['', [Validators.required]],
+      price:[0, [Validators.required]],
+      image: [''],
+      description: ['', [Validators.required]]
+    })
+  }
+}
