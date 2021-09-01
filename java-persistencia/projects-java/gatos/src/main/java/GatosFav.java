92a82fce4f256ac73876ce53afe8484d78d72cc8
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-09-01 01:31:55 -0500

    [add] list fav

---
+++ java-persistencia/projects-java/gatos/src/main/java/GatosFav.java
@@ -0,0 +1,38 @@
+public class GatosFav {
+    String id;
+    String image_id;
+    String apikey = "a21fcf9e-d182-4d45-925a-36cedd0a3964";
+    Imagex image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(String image_id) {
+        this.image_id = image_id;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public Imagex getImage() {
+        return image;
+    }
+
+    public void setImage(Imagex image) {
+        this.image = image;
+    }
+}
--- java-persistencia/projects-java/gatos/src/main/java/Inicio.java
+++ java-persistencia/projects-java/gatos/src/main/java/Inicio.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Inicio {
     public static void main(String[] args) throws IOException {
         int opcion_menu = -1;
-        String[] botones = {"1. ver gatos", "2. salir"};
+        String[] botones = {"1. ver gatos", "2. ver favoritos", "3. salir"};
         do {
             //Menu principal
             String opcion = (String) JOptionPane.showInputDialog(null, "Gatitos Java", "Menu Principal", JOptionPane.INFORMATION_MESSAGE, null, botones, botones[0]);
@@ -18,6 +18,9 @@ public class Inicio {
                 case 0:
                     GatosService.verGatos();
                     break;
+                case 1:
+                    Gatos gato = new Gatos();
+                    GatosService.verFavorito(gato.getApikey());
                 default:
                     break;
             }
