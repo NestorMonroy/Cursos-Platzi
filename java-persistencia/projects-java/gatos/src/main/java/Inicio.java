67e83c0368e7a9567d361d83b963a1c36ccdc9c7
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-09-01 00:02:39 -0500

    [initial] project with api cats

---
+++ java-persistencia/projects-java/gatos/src/main/java/Inicio.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.io.IOException;
+
+public class Inicio {
+    public static void main(String[] args) throws IOException {
+        int opcion_menu = -1;
+        String[] botones = {"1. ver gatos", "2. salir"};
+        do {
+            //Menu principal
+            String opcion = (String) JOptionPane.showInputDialog(null, "Gatitos Java", "Menu Principal", JOptionPane.INFORMATION_MESSAGE, null, botones, botones[0]);
+            //Validamos que opcion seleciona el usuario
+            for (int i = 0; i < botones.length; i++) {
+                if (opcion.equals(botones[i])){
+                    opcion_menu = i;
+                }
+            }
+            switch (opcion_menu){
+                case 0:
+                    GatosService.verGatos();
+                    break;
+                default:
+                    break;
+            }
+
+        } while (opcion_menu != 1);
+    }
+}
