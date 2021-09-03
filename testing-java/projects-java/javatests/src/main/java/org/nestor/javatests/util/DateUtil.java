728edc392ec55e23aa959c1373755ef27bea160f
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-09-02 21:17:19 -0500

    [add] example year leap

---
+++ testing-java/projects-java/javatests/src/main/java/org/nestor/javatests/util/DateUtil.java
@@ -0,0 +1,13 @@
+package org.nestor.javatests.util;
+
+public class DateUtil {
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+//        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+    }
+}
