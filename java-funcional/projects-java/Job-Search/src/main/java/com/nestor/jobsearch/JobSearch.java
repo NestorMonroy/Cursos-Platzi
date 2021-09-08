136667723cec0691c46bb953d2305fe56c4f8fbc
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-09-08 02:10:06 -0500

    [add] CommanderFunctions

--- java-funcional/projects-java/Job-Search/src/main/java/com/nestor/jobsearch/JobSearch.java
+++ java-funcional/projects-java/Job-Search/src/main/java/com/nestor/jobsearch/JobSearch.java
@@ -1,7 +1,13 @@
 package com.nestor.jobsearch;
 
+import com.beust.jcommander.JCommander;
+import com.nestor.jobsearch.cli.CLIArguments;
+
+import static com.nestor.jobsearch.CommanderFunctions.buildCommanderWithName;
+
 public class JobSearch {
     public static void main(String[] args) {
         System.out.println("Hello job search");
+        JCommander jCommander = buildCommanderWithName("job-search", CLIArguments::newInstance);
     }
 }
